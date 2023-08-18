178b3c7b225853973d5c3d4ca2e0324c111f308a
Rafa <rafaelasimoes2203@mail.com>
2023-08-18 14:49:14 -0300

    cocluido

--- lib/main.dart
+++ lib/main.dart
@@ -19,29 +19,17 @@ class ColorAdjustmentApp extends StatefulWidget {
 }
 
 class _ColorAdjustmentAppState extends State<ColorAdjustmentApp> {
-  int redValue = 255;
+  int redValue = 0;
   int greenValue = 0;
   int blueValue = 0;
-  Color backgroundColor = Colors.white;
-  Color fontColor = Colors.black;
+  Color backgroundColor = Colors.black;
+  Color fontColor = Colors.white;
 
-  void adjustRed(int delta) {
+  void adjustColor(int deltaR, int deltaG, int deltaB) {
     setState(() {
-      redValue = (redValue + delta).clamp(0, 255);
-      updateBackgroundAndFontColor();
-    });
-  }
-
-  void adjustGreen(int delta) {
-    setState(() {
-      greenValue = (greenValue + delta).clamp(0, 255);
-      updateBackgroundAndFontColor();
-    });
-  }
-
-  void adjustBlue(int delta) {
-    setState(() {
-      blueValue = (blueValue + delta).clamp(0, 255);
+      redValue = (redValue + deltaR).clamp(0, 255);
+      greenValue = (greenValue + deltaG).clamp(0, 255);
+      blueValue = (blueValue + deltaB).clamp(0, 255);
       updateBackgroundAndFontColor();
     });
   }
@@ -70,72 +58,51 @@ class _ColorAdjustmentAppState extends State<ColorAdjustmentApp> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      adjustRed(10);
-                      greenValue = 0;
-                      blueValue = 0;
+                  Slider(
+                    value: redValue.toDouble(),
+                    min: 0,
+                    max: 255,
+                    onChanged: (value) {
+                      setState(() {
+                        redValue = value.toInt();
+                        updateBackgroundAndFontColor();
+                      });
                     },
-                    child: Text('+R'),
-                    style: ElevatedButton.styleFrom(primary: Colors.red),
-                  ),
-                  SizedBox(width: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      adjustRed(-10);
-                      greenValue = 0;
-                      blueValue = 0;
-                    },
-                    child: Text('-R'),
-                    style: ElevatedButton.styleFrom(primary: Colors.red),
+                    activeColor: Colors.red,
                   ),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      adjustGreen(10);
-                      redValue = 0;
-                      blueValue = 0;
+                  Slider(
+                    value: greenValue.toDouble(),
+                    min: 0,
+                    max: 255,
+                    onChanged: (value) {
+                      setState(() {
+                        greenValue = value.toInt();
+                        updateBackgroundAndFontColor();
+                      });
                     },
-                    child: Text('+G'),
-                    style: ElevatedButton.styleFrom(primary: Colors.green),
-                  ),
-                  SizedBox(width: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      adjustGreen(-10);
-                      redValue = 0;
-                      blueValue = 0;
-                    },
-                    child: Text('-G'),
-                    style: ElevatedButton.styleFrom(primary: Colors.green),
+                    activeColor: Colors.green,
                   ),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      adjustBlue(10);
-                      redValue = 0;
-                      greenValue = 0;
+                  Slider(
+                    value: blueValue.toDouble(),
+                    min: 0,
+                    max: 255,
+                    onChanged: (value) {
+                      setState(() {
+                        blueValue = value.toInt();
+                        updateBackgroundAndFontColor();
+                      });
                     },
-                    child: Text('+B'),
-                    style: ElevatedButton.styleFrom(primary: Colors.blue),
-                  ),
-                  SizedBox(width: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      adjustBlue(-10);
-                      redValue = 0;
-                      greenValue = 0;
-                    },
-                    child: Text('-B'),
-                    style: ElevatedButton.styleFrom(primary: Colors.blue),
+                    activeColor: Colors.blue,
                   ),
                 ],
               ),
